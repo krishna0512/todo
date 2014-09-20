1afc026eb0031cc4d7d4766a5b1a5a97133e8c32
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2014-09-20 14:02:07 +0530

    versioningInit()

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 #include "include/todo.h"
 #include "include/utility.h"
 
+#define VERSION "0.01"
+
 using namespace std;
 
 int main (int argc, char* argv[]) {
@@ -103,6 +105,9 @@ int main (int argc, char* argv[]) {
 	}
 	else if (arg[1] == "help") {
 		printHelp ();
+	}
+	else if (arg[1] == "version") {
+		cout <<VERSION<<endl;
 	}
 	else if (isNumber (arg[1])) {
 		if (arg[2].length() == 0 || argc < 3) {
